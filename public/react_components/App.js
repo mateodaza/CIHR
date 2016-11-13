c725eee632dc57324dbdf1f5c9647917f7bb9791
Mateo Daza <mateodaza@outlook.com>
2016-11-13 18:41:24 -0500

    Doble //

--- public/react_components/App.js
+++ public/react_components/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './react_components/App'
-import Abouts from './react_components//Abouts'
-import Repos from './react_components//Repos'
-import Repo from './react_components//Repo'
-import About from './react_components//About'
-import Home from './react_components//Home'
+import Abouts from './react_components/Abouts'
+import Repos from './react_components/Repos'
+import Repo from './react_components/Repo'
+import About from './react_components/About'
+import Home from './react_components/Home'
 
 class App extends Component {
 
