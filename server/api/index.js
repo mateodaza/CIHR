106ce5b2590ad6cb71b76da39a2f1947f97d6c36
Sebastian Garrido <sebasgarcep@gmail.com>
2016-11-14 01:09:17 -0500

    Server working and structured

---
+++ server/api/index.js
@@ -0,0 +1,22 @@
+const API = (server, options, next) => {
+  // AQUI VAN AGREGANDO SUS MODULOS
+  const plugins = [
+    require('./example'),
+  ];
+
+  // ESTO DE ABAJO NO SE TOCA
+  const pluginOptions = { };
+  server.register(plugins, pluginOptions, err => {
+    if (err) {
+      return next(err);
+    }
+    return next();
+  });
+};
+
+API.attributes = {
+  name: 'api-registry',
+  version: '0.0.1',
+};
+
+module.exports = API;
---
+++ server/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+const Glue = require('glue');
+const { promisifyCallback } = require('./utils');
+
+const manifest = {
+  server: {
+    app: {
+      // SERVER WIDE CONSTANTS OR GLOBAL VARIABLES GO HERE
+    },
+  },
+  connections: [
+    { port: 80, labels: ['web'] },
+    { port: 8080, labels: ['api']},
+  ],
+  registrations: [
+    { plugin: './web', options: { select: 'web' } },
+    { plugin: './api', options: { select: 'api', routes: { prefix: '/api/v1' } } },
+    { plugin: 'blipp' },
+  ],
+};
+
+const options = {
+  relativeTo: __dirname,
+};
+
+Glue.compose(manifest, options)
+.then(server => {
+  console.log('Preparando servidor...');
+  const initializePromise = new Promise((resolve, reject) => {
+    server.initialize(promisifyCallback(resolve, reject));
+  });
+  const startPromise = () => new Promise((resolve, reject) => {
+    server.start(promisifyCallback(resolve, reject));
+  });
+  return initializePromise
+    .then(() => console.log('Iniciando servidor...'))
+    .then(startPromise)
+    .then(() => console.log('Servidor corriendo...'));
+})
+.catch(err => {
+  console.log('El servidor fallo: ', err);
+  process.exit(1);
+});
