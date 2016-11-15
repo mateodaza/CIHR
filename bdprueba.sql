8e7c7b595903a82eecd89fcba23585ce8d5e6cdb
angav14 <angava_@hotmail.com>
2016-11-14 19:49:52 -0500

    Add files via upload

---
+++ bdprueba.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE proyect;
+CREATE TABLE proyect.Funcionario (
+    funcionario_cod INT,
+    funcionario_pwd INT,
+    funcionario_est INT,
+    funcionario_nombre VARCHAR(255)
+);
+INSERT INTO proyect.Funcionario VALUES("2000","123","1","Juan");
+INSERT INTO proyect.Funcionario VALUES("2001","456","0","Carlos");
+INSERT INTO proyect.Funcionario VALUES("2002","789","0","Andres");
+INSERT INTO proyect.Funcionario VALUES("2003","123","1","NoName");
